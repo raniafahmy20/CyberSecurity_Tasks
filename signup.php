4f4a9b9e4043987a185758919cc317116f9665e6
raniafahmy20 <120285519+raniafahmy20@users.noreply.github.com>
2024-02-07 23:46:57 +0200

    Update signup.php

--- signup.php
+++ signup.php
@@ -7,8 +7,8 @@
 <form action="profile.php" method="post">
 <b> First Name :</b><input type="text" name="firstname"><br><br>
 <b> Last Name : </b><input type="text" name="lastname"><br><br>
-<b>  Email :    </b><input type="email" name="email"><br><br>
-<b> Age :       </b><input type="number" name="age"><br><br>
+<b>  Email :    </b><input type="text" name="email"><br><br>
+<b> Age :       </b><input type="text" name="age"><br><br>
 <b> password :  </b><input type="password" name="password"><br><br>
 <input type="submit" value="submit">
 </form>
